323b91e304b338052cb38c84f83851923685c430
Francesc Busquets <francesc@gmail.com>
2019-03-21 16:10:05 +0100

    Publish wp-show

---
+++ wpshow/precache-manifest.50c120973d871e3dd1fe20f48926392e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "c6e4e271a54dfce19a7d",
+    "url": "./static/js/main.ccfd9a54.chunk.js"
+  },
+  {
+    "revision": "7ef04b9eb863cc19e769",
+    "url": "./static/js/2.ddccf9f9.chunk.js"
+  },
+  {
+    "revision": "c6e4e271a54dfce19a7d",
+    "url": "./static/css/main.3b21c84b.chunk.css"
+  },
+  {
+    "revision": "3464246d53d05d3647133c1baee17cc4",
+    "url": "./index.html"
+  }
+];

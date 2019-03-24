a70021c59ee94a96717bce2442748540d9ba09ae
Francesc Busquets <francesc@gmail.com>
2019-03-24 11:47:59 +0100

    wpshow version 2.1

--- wpshow/precache-manifest.6fb59cc24c6e4118aa912bed9662e325.js
+++ wpshow/precache-manifest.6fb59cc24c6e4118aa912bed9662e325.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "./static/js/runtime~main.d653cc00.js"
   },
   {
-    "revision": "0f150651494bd2086db2",
-    "url": "./static/js/main.6d622c34.chunk.js"
+    "revision": "889e0cc5080ac1e5e04d",
+    "url": "./static/js/main.c721ff8f.chunk.js"
   },
   {
     "revision": "5b7d1af2306257066043",
     "url": "./static/js/2.6e8048a9.chunk.js"
   },
   {
-    "revision": "0f150651494bd2086db2",
+    "revision": "889e0cc5080ac1e5e04d",
     "url": "./static/css/main.4003d043.chunk.css"
   },
   {
-    "revision": "257502116647cb86034bb2bdfaa8060c",
+    "revision": "71861a5f576d0210342a83ec8a1877b9",
     "url": "./index.html"
   }
 ];
--- wpshow/service-worker.js
+++ wpshow/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.84897c26948aa3258e0f434cb751bf81.js"
+  "./precache-manifest.6fb59cc24c6e4118aa912bed9662e325.js"
 );
 
 workbox.clientsClaim();

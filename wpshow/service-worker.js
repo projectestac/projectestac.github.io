7fe38ea05bafd9aa8e56f7a13c105ddd768048f1
Francesc Busquets <francesc@gmail.com>
2019-03-25 20:38:14 +0100

    wpshow version 2.5

--- wpshow/service-worker.js
+++ wpshow/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2b1898c0dd0b8e41b543aa85ffca92d2.js"
+  "./precache-manifest.0803eebe24f3fe30df7b386663010f33.js"
 );
 
 workbox.clientsClaim();

388612c11e70e9874e58786b35725f785993370e
Francesc Busquets <francesc@gmail.com>
2019-05-09 16:57:35 +0200

    Add 'mapa innovació educativa'

---
+++ mapa/precache-manifest.739e41a4b0b436bd7d15edd480a5ba97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "131018e00cc593d59b9b786ccf46c0be",
+    "url": "./index.html"
+  },
+  {
+    "revision": "46cb803c47140ac2f9c1",
+    "url": "./static/css/2.6a3d0ad7.chunk.css"
+  },
+  {
+    "revision": "cda649b9217e69f7bdab",
+    "url": "./static/css/main.b739f07c.chunk.css"
+  },
+  {
+    "revision": "46cb803c47140ac2f9c1",
+    "url": "./static/js/2.1dbb763f.chunk.js"
+  },
+  {
+    "revision": "cda649b9217e69f7bdab",
+    "url": "./static/js/main.e42e25ac.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);

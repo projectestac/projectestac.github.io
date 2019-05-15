82f134e63b8b76ac5374b1e3d4c522988905b10d
Francesc Busquets <francesc@gmail.com>
2019-05-15 10:58:14 +0200

    Mapa 1.0.26 beta

---
+++ mapa/precache-manifest.a251eeac0ea12b15e4b29e81de54e6e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48af811a67ff352fa9b1a9b6b77d7c98",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7fb1d582350fbfaa50ae",
+    "url": "./static/css/2.6a3d0ad7.chunk.css"
+  },
+  {
+    "revision": "2ea9777b09b4a82f8eff",
+    "url": "./static/css/main.1fe7272b.chunk.css"
+  },
+  {
+    "revision": "7fb1d582350fbfaa50ae",
+    "url": "./static/js/2.3e9c780f.chunk.js"
+  },
+  {
+    "revision": "2ea9777b09b4a82f8eff",
+    "url": "./static/js/main.157aa9a3.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);

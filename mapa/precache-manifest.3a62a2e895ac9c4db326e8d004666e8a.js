39e39ed7f316d7a1eabd2f29aa7142733522d16c
Francesc Busquets <francesc@gmail.com>
2019-05-17 11:32:45 +0200

    Mapa v1.0.4

--- mapa/precache-manifest.3a62a2e895ac9c4db326e8d004666e8a.js
+++ mapa/precache-manifest.3a62a2e895ac9c4db326e8d004666e8a.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "17bc19f69691d47242cad2d9e83377b7",
+    "revision": "005aab515d4b7cb344eed65bc5790575",
     "url": "./index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/css/2.6a3d0ad7.chunk.css"
   },
   {
-    "revision": "551813744bf1af70502c",
+    "revision": "a450634a02b5aababd38",
     "url": "./static/css/main.1e137aaf.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.3f24f2d1.chunk.js"
   },
   {
-    "revision": "551813744bf1af70502c",
-    "url": "./static/js/main.f51b01f5.chunk.js"
+    "revision": "a450634a02b5aababd38",
+    "url": "./static/js/main.d58faa76.chunk.js"
   },
   {
     "revision": "8c97409f0ee389fe75da",
